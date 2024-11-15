bbe3a632051987deb448ecaa3b3269b10fb414e0
NoobinOut <bigboi@duck.com>
2024-11-15 17:16:34 -0500

    quick fixes that cam helped with

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ArmSystem.java
@@ -120,7 +120,7 @@ public class ArmSystem extends OpMode {
 
 
     //controls the wrist. Takes in an up(D-pad Up), down(D-Pad Down), and variable to go to half position(Binded to X-button)
-    public void ControlWrist(boolean Up, boolean Down, boolean Half){
+    public void ControlWrist(boolean Up, boolean Down, boolean Half, boolean half2){
         // if D-Pad Up being pressed increses the servo position by increment
         if (Up) {
             // Keep stepping up until we hit the max value.
@@ -132,13 +132,14 @@ public class ArmSystem extends OpMode {
             positionWrist -= INCREMENT ;
         }
         //if x-button being pushed move to half max position
-        if(Half){   positionWrist = MAX_POS/2; }
+        if(Half){   positionWrist = 0.17; }
+        if(half2){   positionWrist = 0.52; }
         //send new position to servo
         servoWrist.setPosition(positionWrist);
     }
 
     //controls the Arm. Takes in an up(Y button), down(A button), and variable to go to half position(Binded to X-button)
-    public void ControlArm(boolean Up, boolean Down, boolean Half){
+    public void ControlArm(boolean Up, boolean Down, boolean Half, boolean half2){
         // if Y button being pushed add increment to the arm position if not at max position
         if (Up) {
             // Keep stepping up until we hit the max value.
@@ -154,41 +155,59 @@ public class ArmSystem extends OpMode {
             }
         }
         //if x-button being pushed move to half max position
-        if(Half){   positionArm = MAX_POS/2; }
         //send new position to servo
         servoArm.setPosition(positionArm);
+        if(Half){
+            servoArm.setPosition(0.63);
+        }
+        if(half2){
+            servoArm.setPosition(0.15);
+        }
     }
 
+    //controls the second viper-slide takes in a move up and down command(binded to right-bumper and right-trigger)
     public void restrictedControlArmLift2(boolean moveArmUp, double moveArmDown) {
 
+        //if the right-bumper being pressed turn the slide on at a set power
         if (moveArmUp) {
             motorLiftArm2.setPower(liftArmUpSpeed);
             motorLiftArm2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+            //get the current position of the motor/viper-slide for the hover code
             hoverPoint2 = motorLiftArm2.getCurrentPosition();
+            //if the right-trigger is being pressed turn the slide on at a set power level
+            //the right-trigger outputs a double value where 1 is being pressed fully and 0 is not being pressed
         } else if (moveArmDown != 0.0) {
             motorLiftArm2.setPower(liftArmDownSpeed);
             motorLiftArm2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+            //get the current position of the motor/viper-slide for the hover code
             hoverPoint2 = motorLiftArm2.getCurrentPosition();
         } else {
+            //set the target hover point to the position found above and move the arm at a set speed to hold it
             motorLiftArm2.setTargetPosition(hoverPoint2);
             motorLiftArm2.setPower(liftArmHoverPower);
+            //unlike above we have to use the encoder to be able to run to a set position
             motorLiftArm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         }
     }
 
 
+    //controls the viper-slide takes in a move up and down command(binded to left-bumper and left-trigger)
     public void restrictedControlArmLift(double moveArmUp, boolean moveArmDown){
 
+        //if the right-trigger is being pressed turn the slide on at a set power level
+        //the right-trigger outputs a double value where 1 is being pressed fully and 0 is not being pressed
             if(moveArmUp != 0.0)
             {
                 motorLiftArm.setPower(liftArmUpSpeed);
                 motorLiftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+                //get the current position of the motor/viper-slide for the hover code
                 hoverPoint = motorLiftArm.getCurrentPosition();
             }
+            //if the left-bumper being pressed turn the slide on at a set power
             else if(moveArmDown)
             {
                 motorLiftArm.setPower(liftArmDownSpeed);
@@ -198,8 +217,10 @@ public class ArmSystem extends OpMode {
             }
             else
             {
+                //set the target hover point to the position found above and move the arm at a set speed to hold it
                 motorLiftArm.setTargetPosition(hoverPoint);
                 motorLiftArm.setPower(liftArmHoverPower);
+                //unlike above we have to use the encoder to be able to run to a set position
                 motorLiftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/RobotTeleopMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/RobotTeleopMain.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.subsystems.VisionHandler;
 
 @TeleOp(name = "TeleopMain")
 public class RobotTeleopMain extends OpMode {
+    //drive motors
     DcMotor motorFrontRight;
     DcMotor motorBackRight;
     DcMotor motorFrontLeft;
@@ -20,6 +21,7 @@ public class RobotTeleopMain extends OpMode {
     VisionHandler visionSystem;
     //WebcamName webcam;
 
+    //both arms servos and motors
     DcMotor motorLiftArm;
     DcMotor motorLiftArm2;
     Servo servoGrabber;
@@ -28,18 +30,25 @@ public class RobotTeleopMain extends OpMode {
 
 
 
+    //drive system(the one in subsystems)
     MechanumDrive driveSystem;
+    //the arm system(there are two)
     org.firstinspires.ftc.teamcode.drive.ArmSystem ArmSystem;
 
 
     //private VisionPortal visionPortal;
 
+
+    //all this is called when the init button is pressed
     @Override
     public void init() {
+        //setup the drive motors
         motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
         motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
         motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
         motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        //setup both arm servos and motors
         motorLiftArm = hardwareMap.dcMotor.get("motorLiftArm");
         motorLiftArm2 = hardwareMap.dcMotor.get("motorLiftArm2");
         servoGrabber = hardwareMap.servo.get("servoGrabber");
@@ -47,6 +56,7 @@ public class RobotTeleopMain extends OpMode {
         servoArm = hardwareMap.servo.get("servoArm");
         //webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
 
+        //init the drive and arm system
         driveSystem = new MechanumDrive(motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight);
         ArmSystem = new ArmSystem(motorLiftArm, servoGrabber, motorLiftArm2,servoWrist,servoArm);
         //ArmSystem = new ArmSystem(motorLiftArm, servoGrabber, motorLiftArm2, servoArm);
@@ -54,37 +64,34 @@ public class RobotTeleopMain extends OpMode {
 
         //visionPortal = VisionPortal.easyCreateWithDefaults(
         //        hardwareMap.get(WebcamName.class, "Webcam 1"))
+        //sends a message to driver HUB that all is 'ight
         telemetry.addData("Main","All Systems Online. Here We Go!");
     }
 
     @Override
     public void loop()
     {
-        // First parameter: Strafe x input
-        // Second parameter: Strafe y input
-        // Third parameter: Rotation x input
-        // Fourth parameter: Slow Button
+        // Drives the robot
         driveSystem.drive(-gamepad1.left_stick_x, gamepad1.left_stick_y, -gamepad1.right_stick_x, gamepad1.right_trigger);
 
-        // First parameter: armUp input
-        // Second parameter: armDown input
+        //controls both viper-slides and the gripper
         ArmSystem.restrictedControlArmLift(gamepad2.left_trigger, gamepad2.left_bumper);
         ArmSystem.restrictedControlArmLift2(gamepad2.right_bumper, gamepad2.right_trigger);
         ArmSystem.ControlGripper(gamepad2.b);
 
-        // First parameter: grabberOpen input
-        // Second parameter: grabberClose input
-        ArmSystem.ControlArm(gamepad2.y, gamepad2.a, gamepad2.x);
+        // controls the arm
+        ArmSystem.ControlArm(gamepad2.y, gamepad2.a, gamepad2.x, gamepad2.dpad_left);
 
-        // First parameter: wristFlat input
-        // Second parameter: wristAngled input
-        ArmSystem.ControlWrist(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.x);
+        // controls the wrist
+        ArmSystem.ControlWrist(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.x, gamepad2.dpad_left);
 
+        //sends the data from the arm-system to the driver HUB
         telemetry.addData("Hover",ArmSystem.getHoverPoint());
         telemetry.addData("Hover2",ArmSystem.getHoverPoint2());
         telemetry.addData("Grabber",ArmSystem.getPositionGrabber());
         telemetry.addData("Wrist",ArmSystem.getPositionWrist());
         telemetry.addData("Arm",ArmSystem.getPositionArm());
+        //call a telemetry update to push new data to driver HUB
         telemetry.update();
     }
 
